ca79277bba8e22dba20a7c894964d9c65e39d51e
Emma Paris <mayankpurohit2012@gmail.com>
2020-09-29 15:04:43 +0530

    [Mayank] Add Welcome message

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Snake_And_Ladders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Virtual Snake & Ladders!!");
+        }
+    }
+}
