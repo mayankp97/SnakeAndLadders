865661711c08ab4f7b5ce063a429a21a4245f836
Emma Paris <mayankpurohit2012@gmail.com>
2020-09-29 15:38:50 +0530

    [Mayank] Add Option Function for Snake Or Ladder

--- Program.cs
+++ Program.cs
@@ -8,14 +8,35 @@ namespace Snake_And_Ladders
         {
             Console.WriteLine("Welcome to the Virtual Snake & Ladders!!");
 
-            var positionPlayer1 = 0;
+            var positionOfPlayer1 = 0;
 
             var NumRoll = RollTheDie();
+
+            SnakeOrLadder(ref positionOfPlayer1,NumRoll);
+            Console.WriteLine(positionOfPlayer1);
+            
         }
 
         public static int RollTheDie()
         {
             return new Random().Next(1, 7);
+        }
+        public static void SnakeOrLadder(ref int positionOfPlayer,int NumRoll)
+        {
+            string[] options = new string[] { "No Play", "Snake", "Ladder" };
+            var option = options[new Random().Next(0, 3)];
+            switch (option)
+            {
+                case "Snake":
+                    positionOfPlayer -= NumRoll;
+                    break;
+                case "Ladder":
+                    positionOfPlayer += NumRoll;
+                        break;
+
+                default:
+                        break;
+            }
         }
     }
 }
